7ed7745d84542b2401ddc4a2274c1949338b1e65
Josh Kingsley <josh@joshkingsley.me>
2023-08-29 16:20:05 +0300

    Run commands from Procfile

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+use std::{process::{Command, Child}, fs};
+
+use regex::Regex;
+
 fn main() {
-    println!("Hello, world!");
+    let procfile = fs::read_to_string("Procfile")
+        .expect("Could not open Procfile");
+
+    let re = Regex::new(r"\s*([^:]+):\s*(.+)").unwrap();
+
+    let processes: Vec<_> = re
+        .captures_iter(&procfile)
+        .map(|c| c.extract())
+        .map(|(_, [name, command])| (name, command))
+        .collect();
+
+    let children: Vec<Child> = processes
+        .into_iter()
+        .map(|(_name, command)| {
+            Command::new("bash")
+                .args(["-c", command])
+                .spawn()
+                .expect("Failed to spawn child")
+        })
+        .collect();
+
+    for mut child in children {
+        child.wait().unwrap();
+    }
 }
