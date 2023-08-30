d2ce5c4fe4fbcdc6b09ca3ee409cdff48c300014
Josh Kingsley <josh@joshkingsley.me>
2023-08-30 23:13:00 +0300

    Print on exit

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
-use std::{process::{Command, Child, Stdio}, fs, thread, io::{BufReader, BufRead}, str::FromStr};
+use std::io::{BufReader, BufRead};
+use std::fs;
+use std::thread;
+use std::process::{Command, Stdio};
+use std::error::Error;
 
 use regex::Regex;
 
-fn main() {
+fn main() -> Result<(), Box<dyn Error>> {
     let procfile = fs::read_to_string("Procfile")
         .expect("Could not open Procfile");
 
@@ -14,35 +18,63 @@ fn main() {
         .map(|(_, [name, command])| (name, command))
         .collect();
 
-    let mut children: Vec<_> = processes
-        .into_iter()
+    let name_max_length = processes.iter().fold(0, |acc, (name, _)| std::cmp::max(name.len(), acc));
+
+    let exit_threads: Vec<_> = processes
+        .iter()
         .map(|(name, command)| {
-            (String::from_str(name).unwrap(),
-             Command::new("bash")
-             .args(["-c", command])
-             .stdout(Stdio::piped())
-             .stderr(Stdio::piped())
-             .spawn()
-             .unwrap())
+            let mut child = Command::new("bash")
+                .args(["-c", command])
+                .stdout(Stdio::piped())
+                .stderr(Stdio::piped())
+                .spawn()
+                .expect("Could not run bash");
+
+            let mut stdout = BufReader::new(child.stdout.take().unwrap());
+            let mut stderr = BufReader::new(child.stderr.take().unwrap());
+
+            let name_padded = format!("{:width$}", name, width=name_max_length);
+            let name_padded2 = name_padded.clone();
+            let name_padded3 = name_padded.clone();
+
+            thread::spawn(move || {
+                let mut line = String::new();
+
+                loop {
+                    line.clear();
+                    let len = stdout.read_line(&mut line).unwrap();
+                    if len == 0 {
+                        break
+                    }
+                    println!("{name_padded} | {}", line.trim_end());
+                }
+            });
+
+            thread::spawn(move || {
+                let mut line = String::new();
+
+                loop {
+                    line.clear();
+                    let len = stderr.read_line(&mut line).unwrap();
+                    if len == 0 {
+                        break
+                    }
+                    println!("{name_padded2} | {}", line.trim_end());
+                }
+            });
+
+            return thread::spawn(move || {
+                match child.wait() {
+                    Ok(status) => println!("{name_padded3} | exited with {status}"),
+                    Err(_) => todo!(),
+                }
+            });
         })
         .collect();
 
-    for (name, child) in children.as_mut_slice() {
-        let mut stdout = BufReader::new(child.stdout.take().unwrap());
-        let name = name.clone();
-        thread::spawn(move || {
-            loop {
-                let mut line = String::new();
-                let len = stdout.read_line(&mut line).unwrap();
-                if len == 0 {
-                    return
-                }
-                println!("{name} | {}", line.trim_end());
-            }
-        });
+    for t in exit_threads {
+        let _ = t.join();
     }
 
-    for (_, mut child) in children {
-        child.wait().unwrap();
-    }
+    Ok(())
 }
